c2d03331097c89627db834efe897e3014ae47e49
Alysson Morais <amoraiswk@gmail.com>
2018-04-07 19:35:16 -0300

    Organizando

---
+++ 2018-1-Codigos/MetodologiaLinguagemProgramacao/unipe-estagio02/src/br/unipe/cc/aula06/UsaServico.java
@@ -0,0 +1,12 @@
+package br.unipe.cc.aula06;
+
+public class UsaServico {
+	public static void main(String[] args) {
+		
+		JavaDoc api = new JavaDoc();
+		
+		api.consultarAluno("asdsds");
+		
+	}
+
+}
---
+++ 2018-1-Codigos/MetodologiaLinguagemProgramacao/unipe-estagio02/src/br/unipe/cc/polimorfismo/Academia.java
@@ -0,0 +1,13 @@
+package br.unipe.cc.polimorfismo;
+
+/**
+ * @author Morais, Alysson
+ */
+
+public class Academia {
+	
+	public void informaNome(Pessoa p) {
+		p.falar();
+	}
+	
+}
