691f9db7bd0b12e2995be072e9786b20e5d13e70
Alysson Morais <amoraiswk@gmail.com>
2018-06-18 23:23:54 -0300

    Atendendo aos requisitos

---
+++ 2018-1-Codigos/MetodologiaLinguagemProgramacao/unipe-projeto-arquitetura/src/classes/RegistradorGeral.java
@@ -0,0 +1,15 @@
+package classes;
+
+public class RegistradorGeral {
+	private int valor;
+
+	public int getValor() {
+		return valor;
+	}
+
+	public void setValor(int valor) {
+		this.valor = valor;
+	}
+	
+	
+}
